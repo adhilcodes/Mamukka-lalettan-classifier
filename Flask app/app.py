0685bb2a76ba33c167c123e7cbeff7ef4dd901c9
Abdul Adhil PK <65992809+adhilcodes@users.noreply.github.com>
2021-05-25 13:06:30 +0530

    adding flask app

---
+++ Flask app/app.py
@@ -0,0 +1,53 @@
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+MODEL_PATH = '/home/adhil/Desktop/Lab/Flask app/model/inceptionv3.h5'
+
+# Load your trained model
+model = load_model(MODEL_PATH)
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x, mode='caffe')
+    preds = model.predict(x)
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        basepath = os.path.dirname("/home/adhil/Desktop/Lab/Flask app/uploads/1mammuka.jpeg")
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+        preds = model_predict(file_path, model)
+        pred_class = decode_predictions(preds, top=1)   
+        result = str(pred_class[0][0][1])               
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
